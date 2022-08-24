2176b27cb16f80a11fb34ca7d02d1e9de09bb061
Sandip <sandipghoshany@gmail.com>
2022-08-24 16:13:03 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,255 @@
+# Author : Sandip Kumar Ghosh
+# Date : 12th January 2022
+# Place : Pune
+
+# print("hello world!")
+
+# y = 10 + 20;
+# print(y)
+
+# name = "Sandip Kumar Ghosh"
+# print(name)
+
+# x = 10;
+# z = 25;
+
+# sum = x + z;
+# print(sum)
+
+# print("Okay man this is good")
+# # this is comment bro
+
+# print('''Sandip is a good boy
+# he is into some sports
+# but that is not so good
+# but let's see what happens next''')
+
+
+# str = '''Hello bro
+# let's understand this thing'''
+# print(str)
+
+# from playsound import playsound 
+# playsound('C:\\Users\\Lenovo\\OneDrive\\Documents\\Sound recordings\\Recording.mp3')
+
+# import os
+# print(os.listdir())
+
+# from operator import truediv
+
+
+# x = "Sandip"
+# y = 123
+# z = True
+# k = None
+
+# print(x)
+# print(y)
+# print(z)
+# print(k)
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(k))
+
+# x = 10
+# y = 20
+# print("the sum is " , x + y)
+
+# x = "123"
+# print(x)
+# print(type(x))
+# x = int(x)
+# print(x)
+# print(type(x))
+
+# x = input("Enter your name : ")
+# print(x)
+# print(type(x))
+
+
+# y = input("Enter something else : ")
+# print(y)
+# print(type(y))
+# y = int(y)
+# print(type(y))
+
+# x = input("enter the first number : ")
+# y = input("enter the second number")
+# x = int(x)
+# y = int(y)
+# avg = (x + y)/2
+# print("Avergae is = ", avg)
+
+# x = int(input("enter the first number : "))
+# y = int(input("enter the second number : "))
+# avg = (x + y)/2
+# print("Avergae is = ", avg)
+
+# x = "Sandip"
+# y = 'Hello'
+# z = '''Bye Bye'''
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# name = "Mani"
+# print("hello")
+# print(name)
+
+# print("----0")
+# print("||||")
+
+# birth_year = int(input("what is your birth year? "))
+# age = 2022 - birth_year
+# # print("Your age is " + age)
+# print(age)
+
+# weight_in_pounds = int(input("What is the weight?(in pounds) "))
+# weight_in_kg = weight_in_pounds * 0.453592
+# print(weight_in_kg)
+
+# name = 'Sandip\'s Laptop'
+# print(name)
+
+# bio = '''Hello this is
+#         Sandip Kumar Ghosh
+#         from Odisha
+#         B.Tech from CET BBSR'''
+# print("Bio : " + bio)
+
+# bio = '''Hello this is
+# Sandip Kumar Ghosh
+# from Odisha
+# B.Tech from CET BBSR'''
+# print(bio)
+
+
+# email_msg = '''
+# Hello, Sandip
+# This is about the subscription.
+# Get your soon.
+
+# From,
+# Team Bulbul
+# '''
+# print(email_msg)
+
+# name = "SANDIP"
+# print(name[0])
+# print(name[3])
+# print(name[-1])
+# print(name[-3])
+
+# name = "GHOSH"
+# print(name[0:3])
+# print(name)
+# print(name[-3:-1])
+
+# first_name = "Sandip"
+# last_name = "Ghosh"
+# msg = first_name + " [" + last_name + "] " + "is a coder!"
+# print(msg)
+
+# a = 10
+# b = 20
+
+# if a > b:
+#     print("True")
+#     print("Okay fine")
+# else :
+#     print("False")
+#     print("This is good")
+
+# print("But no") 
+
+'''
+Hello World
+But this is the same thing
+about the thing
+'''
+"""
+That can be one in this way
+but this is the thing
+that can be done 
+"""
+
+# x = 10
+# y = int(20)
+# z = str(30)
+# k = float(40)
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+# print(k)
+# print(type(k))
+
+# x = y = z = "Sandip"
+# print(x)
+# print(y)
+# print(z)
+# y = "Ghosh"
+# print(x)
+# print(y)
+# print(z)
+
+# fruits = ["apple", "banana", "cherry"]
+# x, y, z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+# x = 14
+# y = "Sandip"
+# print(x)
+# print(y)
+# # print(x + y)
+# print(x, y)
+
+# import random
+
+# x = "HellWorld"
+# print(len(x))
+
+# for x in "SandipGhosh" :
+#     print(random.randrange(1,100))
+    
+# print('and' in x)
+# if 'hos' in x :
+#     print("It is present")
+
+# if 'hello' not in x :
+#     print("It is okay")
+
+# x = "SandipGhosh"
+# print(x[1:5])
+# print(x[1:])
+# print(x[:5])
+# print(x[:])
+# print(x[-5:-2])
+
+# print(x.upper())
+# print(x.lower())
+
+# print(x.strip())
+# print(x.replace('S',"J"))
+# print(x)
+# print(x.split(','))
+# y = " is a good boy"
+# print(x + " okay boss" + y)
+
+# x = "Sandip {}"
+# y = 23
+# print(x.format(23))
+# print(x)
+
+# def getSum(x,y) :
+#     return  x + y
+
+# print(getSum(10,23))
+# print("Hello")
